01e30a98bc4e678e0212169e73b9d9c64b804678
Naqibullah <naqibullah1400@gmail.com>
2022-12-09 14:26:59 +0430

    294 delivery methods complete

--- app/Http/Controllers/Admin/Market/DeliveryController.php
+++ app/Http/Controllers/Admin/Market/DeliveryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin\Market;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Market\Delivery\StoreDeliveryRequest;
+use App\Http\Requests\Admin\Market\Delivery\UpdateDeliveryRequest;
+use App\Models\Market\Delivery;
 use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
@@ -14,7 +17,8 @@ class DeliveryController extends Controller
      */
     public function index()
     {
-        return view('admin.market.delivery.index');
+        $delivery_methods = Delivery::orderBy('id', 'desc')->get();
+        return view('admin.market.delivery.index', compact('delivery_methods'));
     }
 
     /**
@@ -33,9 +37,10 @@ class DeliveryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreDeliveryRequest $request)
     {
-        //
+        Delivery::create($request->all());
+        return redirect()->route('delivery.index')->with('swal-success', 'روش ارسال جدید با موفقیت ثبت شد');
     }
 
     /**
@@ -57,7 +62,8 @@ class DeliveryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $delivery_method = Delivery::FindOrFail($id);
+        return view('admin.market.delivery.edit', compact('delivery_method'));
     }
 
     /**
@@ -67,9 +73,11 @@ class DeliveryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateDeliveryRequest $request, $id)
     {
-        //
+        $delivery_method = Delivery::FindOrFail($id);
+        $delivery_method->update($request->all());
+        return redirect()->route('delivery.index')->with('swal-success', 'روش ارسال با موفقیت ویرایش شد');
     }
 
     /**
@@ -80,6 +88,8 @@ class DeliveryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $delivery_method = Delivery::FindOrFail($id);
+        $delivery_method->destroy($id);
+        return redirect()->route('delivery.index')->with('swal-success', 'روش ارسال با موفقیت حذف شد');
     }
 }
